521feb2bbc01eb29ae1523dd141c870b59f1831c
Nhico Noel Ortazon <legendarynhico@gmail.com>
2025-08-18 12:32:10 +0800

    using LET in javascript

---
+++ project.js
@@ -0,0 +1,5 @@
+let name = "Nhico";        // a box holding your name (string)
+let age = 25;              // a box holding your age (number)
+let hobby = "Photography"; // a box holding your hobby (string)
+
+console.log(`Hi, my name is ${name}, I am ${age} years old and I love ${hobby}.`);
